67e6835d8a78d72d3992339d6362477840caca2d
Admin <Admin@DESKTOP-T8Q79FD>
2021-04-08 15:51:56 +0700

    first commit

---
+++ StackLayoutExercise1/StackLayoutExercise1/StackLayoutExercise1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayoutExercise1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StackPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
